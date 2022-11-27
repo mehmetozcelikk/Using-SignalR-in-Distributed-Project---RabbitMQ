f672174ff7fc1a5490c634b65f809b1b8945bf81
Usternder <mehmett.ozcelik@yahoo.com>
2022-11-27 15:42:56 +0300

    add Swagger

--- ApiExamples/Startup.cs
+++ ApiExamples/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.OpenApi.Models;
 
 namespace ApiExamples
 {
@@ -18,6 +18,10 @@ namespace ApiExamples
              .SetIsOriginAllowed(x=>true)
             ));
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiExamples.ApiExamples", Version = "v1" });
+            });
             services.AddSignalR();
             services.AddControllers();
         }
@@ -31,6 +35,12 @@ namespace ApiExamples
 
             app.UseCors();
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiExamples.ApiExamples v1");
+
+            });
 
             app.UseEndpoints(endpoints =>
             {
